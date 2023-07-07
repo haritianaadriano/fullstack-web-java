67ed14145f12bd31af652f27d96e5750d96b7ec0
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-07-07 09:56:36 +0300

    chore: homework finish with cookie

--- src/main/java/com/demo/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/demo/thymeleaf/controller/EmployeeController.java
@@ -3,8 +3,6 @@ package com.demo.thymeleaf.controller;
 import com.demo.thymeleaf.model.Employee;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,12 +10,14 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class EmployeeController {
-    private List<jakarta.servlet.http.Cookie> employeesInCookie = new ArrayList<>();
+    private List<Cookie> employeesInCookie = new ArrayList<>();
     private List<Employee> employees = new ArrayList<>();
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -27,17 +27,18 @@ public class EmployeeController {
             BindingResult errors,
             Model model,
             HttpServletResponse response) throws JsonProcessingException {
-        jakarta.servlet.http.Cookie newEmployee = new jakarta.servlet.http.Cookie(
-                (""+employee.getId()),
-                objectMapper.writeValueAsString(employee));
+        Cookie newEmployee = new Cookie(
+                String.valueOf(employee.getId()),
+                String.format("%d-%s", employee.getId(), employee.getFirstname()));
         employees.add(employee);
         response.addCookie(newEmployee);
         employeesInCookie.add(newEmployee);
         return "redirect:/employee";
     }
 
+
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
-    public String employeeResolver(Model model, HttpServlet servlet) {
+    public String employeeResolver(Model model) {
         model.addAttribute("employees", employees);
         model.addAttribute("employee", new Employee());
         return "index";
